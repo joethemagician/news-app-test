e7de985a24cceeee15314cd2d9498d50f8869d00
Joe Stone <joesif_uk@yahoo.co.uk>
2024-07-22 13:38:30 +0100

    Update country names on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main>
       <h1 className="text-lg mb-5">News Headlines</h1>
       <ul>
-          <li><a href="/headlines/gb">UK</a></li>
-          <li><a href="/headlines/us">US</a></li>
+          <li><a href="/headlines/gb">United Kingdom</a></li>
+          <li><a href="/headlines/us">United States</a></li>
           <li><a href="/headlines/fr">France</a></li>
           <li><a href="/headlines/au">Australia</a></li>
           <li><a href="/headlines/in">India</a></li>
