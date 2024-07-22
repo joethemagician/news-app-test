842523abe0bc6fcb899d053847c54fbd275fe9a9
Joe Stone <joesif_uk@yahoo.co.uk>
2024-07-22 13:03:58 +0100

    Add fetch from newsapi

--- app/headlines/[country]/page.tsx
+++ app/headlines/[country]/page.tsx
@@ -1,4 +1,20 @@
-export default function HeadlinesPage({ params }) {
+export default async function HeadlinesPage({ params }) {
   const countyCode = params.country;
-  return <p>Country Code: { countyCode }</p>
+
+  
+  const articlesResponse = await fetch(`https://newsapi.org/v2/top-headlines?pageSize=5&country=${countyCode}&apiKey=${process.env.NEWS_API_KEY}`);
+  const articlesResponseJson = await articlesResponse.json();
+
+  const articles = articlesResponseJson.articles;
+
+  return (
+    <main>
+      <p>Country Code: { countyCode }</p>
+      <ul>
+        {articles.map(article => (<li key={article.title}>{article.title}</li>))}
+      </ul>
+    </main>
+  );
+
+
 }
